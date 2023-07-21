77f1aba81405da45a6d2c9c5f0e0dfd8649a4ae4
Arpit Kast <Arpit Kast>
2023-07-21 19:23:18 +0530

    fix

--- app.js
+++ app.js
@@ -6,9 +6,11 @@ const {mongoose} = require('./db/mongoose');
 
 const bodyParser = require('body-parser')
 
+// var myEmitter = new (require('events').EventEmitter)();
+
 //Load in the mongoose models
 
-const {List, Task} = require('./db/models/index');
+const {List, Task, User} = require('./db/models/index');
 // const {List} = require('./db/models/list.model')
 // const {Task} = require('./db/models/task.model')
 
@@ -18,10 +20,63 @@ app.use(bodyParser.json());
 //cors headers middleware
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Methods", "GET, POST, HEAD, OPTIONS, PUT, PATCH, DELETE");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Expose-Headers','x-access-token, x-refresh-token')
     next();
   });
 
+
+// Verify Refresh Token Middleware (which will be verifying the session)
+let verifySession = (req, res, next) => {
+    // grab the refresh token from the request header
+    let refreshToken = req.header('x-refresh-token');
+
+    // grab the _id from the request header
+    let _id = req.header('_id');
+
+    User.findByIdAndToken(_id, refreshToken).then((user) => {
+        if (!user) {
+            // user couldn't be found
+            return Promise.reject({
+                'error': 'User not found. Make sure that the refresh token and user id are correct'
+            });
+        }
+
+
+        // if the code reaches here - the user was found
+        // therefore the refresh token exists in the database - but we still have to check if it has expired or not
+
+        req.user_id = user._id;
+        req.userObject = user;
+        req.refreshToken = refreshToken;
+
+        let isSessionValid = false;
+
+        user.sessions.forEach((session) => {
+            if (session.token === refreshToken) {
+                // check if the session has expired
+                if (User.hasRefreshTokenExpired(session.expiresAt) === false) {
+                    // refresh token has not expired
+                    isSessionValid = true;
+                }
+            }
+        });
+
+        if (isSessionValid) {
+            // the session is VALID - call next() to continue with processing this web request
+            next();
+        } else {
+            // the session is not valid
+            return Promise.reject({
+                'error': 'Refresh token has expired or the session is invalid'
+            })
+        }
+
+    }).catch((e) => {
+        res.status(401).send(e);
+    })
+}
 app.use(cors());
 app.get("/", (req,res)=>{
 res.send("server working");
@@ -40,6 +95,8 @@ app.get('/lists', (req,res)=>{
    // We want to return an array of all the lists in the database
    List.find({}).then((lists)=>{
     res.send(lists);
+   }).catch((err)=>{
+    console.log(err,"<<<<<<<<<<<<<")
    })
 });
 
@@ -71,6 +128,9 @@ app.patch('/lists/:id', (req,res)=>{
         $set: req.body
     }).then(()=>{
         res.sendStatus(200);
+    }).catch((err)=>{
+        res.send(err)
+        console.log(err,"<<<<<<<<<")
     })
 });
 
@@ -84,6 +144,9 @@ app.delete('/lists/:id', (req,res)=>{
         _id: req.params.id
     }).then(()=>{
         res.send(removedListDoc);
+    }).catch((err)=>{
+        res.send(err)
+        console.log(err,"<<<<<<<<<")
     })
 });
 
@@ -126,7 +189,7 @@ app.patch('/lists/:listId/tasks/:taskId', (req, res) =>{
     },{
         $set: req.body
     }).then(() =>{
-        res.sendStatus(200);
+        res.send({message: 'Updated successfully.'});
     }).catch((err)=>{
         console.log(err)
     })
@@ -145,6 +208,77 @@ app.delete('/lists/:listId/tasks/:taskId', (req, res)=>{
     })
 });
 
+
+/**USER ROUTES */
+
+/**
+ * POST /users
+ * Purpose: Sign up
+ */
+app.post('/users', (req, res)=>{
+    //user sign up
+
+    let body = req.body;
+    let newUser = new User(body);
+
+    newUser.save().then(()=>{
+        return newUser.createSession();
+    }).then((refreshToken)=>{
+        return newUser.generateAccessAuthToken().then((accessToken)=>{
+            return {accessToken, refreshToken}
+        });
+    }).then((authToken)=>{
+        res
+           .header('x-refresh-token', authToken.refreshToken)
+           .header('x-access-token', authToken.accessToken)
+           .send(newUser);
+    }).catch((e)=>{
+        res.status(400).send(e);
+    })
+});
+
+
+/**
+ * POST /user/login
+ * Purpose: Login
+ */
+app.post('/users/login', (req,res)=>{
+    let email = req.body.email;
+    let password = req.body.password;
+
+    User.findByCredentials(email, password).then((user)=>{
+        return user.createSession().then((refreshToken)=>{
+            return user.generateAccessAuthToken().then((accessToken)=>{
+                return {accessToken, refreshToken}
+            });
+        }).then((authToken)=>{
+            res
+            .header('x-refresh-token', authToken.refreshToken)
+            .header('x-access-token', authToken.accessToken)
+            .send(user);
+        }).catch((e)=>{
+            res.status(400).send(e);
+        })
+    })
+})
+
+/**
+ * GET /users/me/access-token
+ * Purpose: generates and return an access token
+ */
+
+app.get('/users/me/access-token', verifySession, (req, res) => {
+    // we know that the user/caller is authenticated and we have the user_id and user object available to us
+    req.userObject.generateAccessAuthToken().then((accessToken) => {
+        res.header('x-access-token', accessToken).send({ accessToken });
+    }).catch((e) => {
+        res.status(400).send(e);
+    });
+})
+
+
 app.listen(3000, ()=>{
     console.log("Server is listening on port 3000");
 })
+
+// myEmitter.emit('error', new Error('foobar'));
